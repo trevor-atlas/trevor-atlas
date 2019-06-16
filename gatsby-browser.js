57db53303632259dcf18de5c873701ff0e933218
Trevor Allen <trevoristall@gmail.com>
2019-06-16 15:55:23 -0400

    rewrite

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+// custom typefaces
+// import React from 'React''
+import 'prismjs/themes/prism-tomorrow.css'
+import "typeface-montserrat"
+import "typeface-merriweather"
+// export const wrapPageElement = ({ element, props }) => {
+//     // props provide same data to Layout as Page element will get
+//     // including location, data, etc - you don't need to pass it
+//     return <div {...props}>{element}</div>
+// }
---
+++ src/components/bio.tsx
@@ -0,0 +1,79 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from 'React'
+import { StaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+import showdown from 'showdown';
+
+const converter = new showdown.Converter();
+
+import { rhythm } from "../utils/typography"
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social, description } = data.site.siteMetadata
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              style={{
+
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                minWidth: 100,
+                borderRadius: `100%`,
+              }}
+              imgStyle={{
+                borderRadius: `50%`,
+              }}
+            />
+            <div>
+            <div dangerouslySetInnerHTML={{__html: converter.makeHtml(description)}}></div>
+              <a href={`https://twitter.com/${social.twitter}`}>
+                You should follow him on Twitter
+                </a>
+              </div>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile.jpg/" }) {
+      childImageSharp {
+        fixed(width: 100, height: 100) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        description
+        author
+        social {
+          twitter
+          github
+        }
+      }
+    }
+  }
+`
+
+export default Bio
