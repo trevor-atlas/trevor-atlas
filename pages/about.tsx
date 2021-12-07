9d81809a3ef2cc686b05352a1217c1df40671eed
Trevor Atlas <me@trevoratlas.com>
2021-12-06 19:34:53 -0500

    bad prop

--- pages/about.tsx
+++ pages/about.tsx
@@ -130,7 +130,7 @@ const About: FC<IAbout> = ({ top }) => (
               width="100%"
               height="166"
               scrolling="no"
-              frameborder="no"
+              frameBorder="no"
               allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/98042245&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
             ></iframe>
