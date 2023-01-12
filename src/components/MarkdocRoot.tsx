293d2cf77975b724fa2caf443f490fa33dae0508
Trevor Atlas <me@trevoratlas.com>
2023-01-12 00:01:53 -0500

    maybe?

--- src/components/MarkdocRoot.tsx
+++ src/components/MarkdocRoot.tsx
@@ -4,7 +4,11 @@ import markdocConfig from '../../lib/markdocConfig';
 import { OGMap } from '../../lib/opengraph-scraper';
 
 export default function MarkdocRoot({ ast }: { ast: RenderableTreeNodes }) {
-  return Markdoc.renderers.react(ast, React, {
-    components: markdocConfig.getComponents()
-  });
+  return (
+    <>
+      {Markdoc.renderers.react(ast, React, {
+        components: markdocConfig.getComponents()
+      })}
+    </>
+  );
 }
